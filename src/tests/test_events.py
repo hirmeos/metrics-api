6701a7228031c838616c0df98c9090951a775e54
Javier Arias <javier@arias.re>
2019-08-21 17:24:53 +0100

    test all endpoints

--- src/tests/test_events.py
+++ src/tests/test_events.py
@@ -50,6 +50,16 @@ class TestEvents():
         assert_equals(results["data"][0]["work_uri"], uri)
         ok_(results["data"])
 
+    def test_get_all_events_from_fake_uri(self):
+        uri = "info:doi:10.11647/obp.FAKE"
+        params = {"filter": "work_uri:{}".format(uri)}
+        res = self.app.get('/events', params, status=404)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 404)
+        assert_equals(results["status"], "error")
+        assert_equals(results["message"],
+                      "No records have matched your search criteria.")
+
     def test_get_all_events_from_uri_and_start_date(self):
         uri = "info:doi:10.11647/obp.0002"
         params = {"filter": "work_uri:{}".format(uri),
@@ -75,6 +85,169 @@ class TestEvents():
         assert_equals(results["data"][0]["timestamp"],
                       "2019-01-01T00:00:00+0000")
         ok_(results["data"])
+
+    def test_get_from_uri_aggregate_broken(self):
+        uri = "info:doi:10.11647/obp.0001"
+        params = {"filter": "work_uri:{}".format(uri), "aggregation": "fake"}
+        res = self.app.get('/events', params, status=400)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 400)
+        assert_equals(results["status"], "error")
+        assert_equals(results["message"], "Invalid parameters provided.")
+
+    def test_get_from_uri_aggregate_measure(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "measure_uri"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 4)
+        expected = {
+            "Google Books": 3,
+            "OAPEN": 3,
+            "Open Book Publishers HTML Reader": 3,
+            "World Reader": 6
+        }
+        for value in results["data"]:
+            assert_equals(value["value"], expected[value["source"]])
+
+    def test_get_from_uri_aggregate_measure_country(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "measure_uri,country_uri"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 4)
+        expected = {
+            "Google Books": 1,
+            "OAPEN": 2,
+            "Open Book Publishers HTML Reader": 3,
+            "World Reader": 2
+        }
+        for value in results["data"]:
+            assert_equals(len(value["data"]), expected[value["source"]])
+
+    def test_get_from_uri_aggregate_country_measure(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "country_uri,measure_uri"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 7)
+        expected = {
+            "Australia": 1,
+            "Cuba": 1,
+            "United Kingdom": 1,
+            "New Zealand": 1,
+            "Puerto Rico": 1,
+            "United States of America": 1,
+            None: 2
+        }
+        for value in results["data"]:
+            assert_equals(len(value["data"]), expected[value["country_name"]])
+
+    def test_get_from_uri_aggregate_measure_year(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "measure_uri,year"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 4)
+        expected = {
+            "Google Books": {"2018": 1, "2019": 2},
+            "OAPEN": {"2018": 1, "2019": 2},
+            "Open Book Publishers HTML Reader": {"2018": 1, "2019": 2},
+            "World Reader": {"2018": 2, "2019": 4}
+        }
+        for measure in results["data"]:
+            assert_equals(len(measure["data"]), 2)
+            for value in measure["data"]:
+                assert_equals(value["value"],
+                              expected[measure["source"]][value["year"]])
+
+    def test_get_from_uri_aggregate_year_measure(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "year,measure_uri"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 2)
+        expected = {
+            "2018": {
+                "Google Books": 1,
+                "OAPEN": 1,
+                "Open Book Publishers HTML Reader": 1,
+                "World Reader": 2
+            },
+            "2019": {
+                "Google Books": 2,
+                "OAPEN": 2,
+                "Open Book Publishers HTML Reader": 2,
+                "World Reader": 4
+            }
+        }
+        for year in results["data"]:
+            assert_equals(len(year["data"]), 4)
+            for value in year["data"]:
+                assert_equals(value["value"],
+                              expected[year["year"]][value["source"]])
+
+    def test_get_from_uri_aggregate_measure_month(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "measure_uri,month",
+                  "start_date": "2019-01-01"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 4)
+        expected = {
+            "Google Books": {"01": 1, "02": 1},
+            "OAPEN": {"01": 1, "02": 1},
+            "Open Book Publishers HTML Reader": {"01": 1, "02": 1},
+            "World Reader": {"01": 2, "02": 2}
+        }
+        for measure in results["data"]:
+            assert_equals(len(measure["data"]), 2)
+            for value in measure["data"]:
+                assert_equals(value["value"],
+                              expected[measure["source"]][value["month"]])
+
+    def test_get_from_uri_aggregate_month_measure(self):
+        uri = "info:doi:10.11647/obp.0002"
+        params = {"filter": "work_uri:{}".format(uri),
+                  "aggregation": "month,measure_uri",
+                  "start_date": "2018-12-01",
+                  "end_date": "2018-12-31"}
+        res = self.app.get('/events', params, status=200)
+        results = json.loads(res.body.decode('utf-8'))
+        assert_equals(results["code"], 200)
+        assert_equals(results["status"], "ok")
+        assert_equals(results["count"], 1)
+        expected = {
+            "12": {
+                "Google Books": 1,
+                "OAPEN": 1,
+                "Open Book Publishers HTML Reader": 1,
+                "World Reader": 2
+            }
+        }
+        for month in results["data"]:
+            assert_equals(len(month["data"]), 4)
+            for value in month["data"]:
+                assert_equals(value["value"],
+                              expected[month["month"]][value["source"]])
 
     def test_post_unauth(self):
         payload = {}
